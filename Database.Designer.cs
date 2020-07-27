4776e62c779b092bab5f748c4ecac33d5687f2bc
kehlert72 <kaleb.ehlert@gmail.com>
2020-07-27 17:15:39 -0400

    Completed project

---
+++ Database.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EhlertProgram11
+{
+    partial class frmDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMembers = new System.Windows.Forms.Label();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnUpdateData = new System.Windows.Forms.Button();
+            this.dgdMember = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgdMember)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMembers
+            // 
+            this.lblMembers.AutoSize = true;
+            this.lblMembers.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.lblMembers.Location = new System.Drawing.Point(207, 43);
+            this.lblMembers.Name = "lblMembers";
+            this.lblMembers.Size = new System.Drawing.Size(50, 13);
+            this.lblMembers.TabIndex = 1;
+            this.lblMembers.Text = "Members";
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.btnLoadData.Location = new System.Drawing.Point(155, 283);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadData.TabIndex = 2;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // btnUpdateData
+            // 
+            this.btnUpdateData.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.btnUpdateData.Location = new System.Drawing.Point(236, 283);
+            this.btnUpdateData.Name = "btnUpdateData";
+            this.btnUpdateData.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdateData.TabIndex = 3;
+            this.btnUpdateData.Text = "Update Data";
+            this.btnUpdateData.UseVisualStyleBackColor = true;
+            this.btnUpdateData.Click += new System.EventHandler(this.btnUpdateData_Click);
+            // 
+            // dgdMember
+            // 
+            this.dgdMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgdMember.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.dgdMember.Location = new System.Drawing.Point(12, 59);
+            this.dgdMember.Name = "dgdMember";
+            this.dgdMember.Size = new System.Drawing.Size(456, 218);
+            this.dgdMember.TabIndex = 4;
+            // 
+            // frmDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 386);
+            this.Controls.Add(this.dgdMember);
+            this.Controls.Add(this.btnUpdateData);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.lblMembers);
+            this.Name = "frmDatabase";
+            this.Text = "Load and Update Database";
+            ((System.ComponentModel.ISupportInitialize)(this.dgdMember)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMembers;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnUpdateData;
+        private System.Windows.Forms.DataGridView dgdMember;
+    }
+}
+
---
+++ Database.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;    // Namespace for classes to use with an Access database
+using System.Windows.Forms;
+
+/*  ID
+ *      Load and Update the Member Database, Kaleb Ehlert, 4/15/2018
+ *  Purpose
+ *      The purpose of the program is to implement GUI that display's a table of information from a database and display it
+ *       on the form. Then allow the user to change data on the database if they want to.
+ *  Form Controls
+ *      lblMember: A label that shows the members of the database.
+ *      dgdMember: A DataGridView that displays the contents/data of the database.
+ *      btnLoadData: A button that loads in the data from the database.
+ *      btnUpdateData: A button that updates the database with the data the user has entered on the form.
+ *  Code & Logic
+ *      The input is either the keyboard text the user enters.
+ *      The processing updates the database with the new information.
+ *      The output is the data from the database.
+ */
+
+namespace EhlertProgram11
+{
+    public partial class frmDatabase : Form
+    {
+        private string connectionString;
+        private string sqlCommand;
+        private OleDbCommandBuilder commandBuild;
+        private OleDbCommand dbCommand;
+        private OleDbConnection dbConnect;
+        private OleDbDataAdapter memberAdapter;
+        private DataSet memberDataSet;
+
+        public frmDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            // Construct an OleDbConnectionobject using the connection string
+            connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=member.accdb";
+            dbConnect = new OleDbConnection(connectionString);
+
+            // Assign the SQL command to select all records ordered by last, then first name
+            sqlCommand = "SELECT * FROM memberTable ORDER BY LastName ASC, FirstName ASC;";
+
+            // Construct an OleDbCommand object to hold the query text
+            // Then tie the command object to the connection object
+            dbCommand = new OleDbCommand();
+            dbCommand.CommandText = sqlCommand;
+            dbCommand.Connection = dbConnect;
+
+            // Construct an OleDbDataAdapter object to exchange data between the source database and the DataSet
+            //  object using the assigned select command
+            memberAdapter = new OleDbDataAdapter();
+            memberAdapter.SelectCommand = dbCommand;
+
+            // Construct a DataSet object and fill with the member database data
+            memberDataSet = new DataSet();
+            memberAdapter.Fill(memberDataSet, "memberTable");
+
+            // Connect the DataGridView to the DataSet
+            dgdMember.DataSource = memberDataSet;
+            dgdMember.DataMember = "memberTable";
+        }
+
+        private void btnUpdateData_Click(object sender, EventArgs e)
+        {
+            // Construct a CommandBuilder object to automatically generate SQL
+            commandBuild = new OleDbCommandBuilder(memberAdapter);
+
+            // Use the DataAdapter's Update method to save changes from the DataSet diusplayed in the DataGridView
+            //  to the source database
+            memberAdapter.Update(memberDataSet, "memberTable");
+        }
+    }
+}
